d55decf9fe9fb1cc8c3a7855320672b6a68db75c
mkao <mkaoibrahim@gmail.com>
2025-07-11 10:08:53 +0300

    feat(core): add navigation contracts

---
+++ core/navigation/src/commonMain/kotlin/iz/mkao/mirasalon/core/navigation/CommonParcelize.kt
@@ -0,0 +1,7 @@
+package iz.mkao.mirasalon.core.navigation
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.BINARY)
+expect annotation class CommonParcelize()
+
+expect interface CommonParcelable
