533e9bbfc58c606ded65e9b9bac4cb2a3b77e9b0
mkao <mkaoibrahim@gmail.com>
2025-08-06 15:04:50 +0300

    feat(chat): add chat domain models and realtime

---
+++ core/navigation/src/commonMain/kotlin/iz/mkao/mirasalon/core/navigation/ChatRoute.kt
@@ -0,0 +1,18 @@
+package iz.mkao.mirasalon.core.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed interface ChatRoute : Route {
+    @Serializable
+    @CommonParcelize
+    data object ChatList : ChatRoute
+
+    @Serializable
+    @CommonParcelize
+    data class ChatDetail(
+        val conversationId: String,
+        val participantName: String? = null,
+        val participantAvatarUrl: String? = null
+    ) : ChatRoute
+}
