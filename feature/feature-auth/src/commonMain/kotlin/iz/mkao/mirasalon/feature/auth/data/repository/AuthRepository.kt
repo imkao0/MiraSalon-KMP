12efc20752b5464ffeedd49517856996873c55c0
mkao <mkaoibrahim@gmail.com>
2025-09-13 22:22:15 +0300

    feat(auth): add auth data layer

---
+++ feature/feature-auth/src/commonMain/kotlin/iz/mkao/mirasalon/feature/auth/data/repository/AuthRepository.kt
@@ -0,0 +1,14 @@
+package iz.mkao.mirasalon.feature.auth.data.repository
+
+import iz.mkao.mirasalon.core.network.model.dto.*
+import iz.mkao.mirasalon.core.network.result.NetworkResult
+
+interface AuthRepository {
+    suspend fun login(request: LoginRequest): NetworkResult<AuthResponse>
+    suspend fun register(request: RegisterRequest): NetworkResult<AuthResponse>
+    suspend fun logout()
+    suspend fun updateProfile(request: UpdateProfileRequest): NetworkResult<Unit>
+    suspend fun deleteAccount(): NetworkResult<Unit>
+    suspend fun saveEmail(email: String?)
+    suspend fun getSavedEmail(): String?
+}
