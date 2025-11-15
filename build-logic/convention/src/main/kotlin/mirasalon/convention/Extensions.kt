b2333d8e1c3a843b1c2db3c81de1f73b0d85fb30
mkao <mkaoibrahim@gmail.com>
2025-11-15 10:35:28 +0300

    chore(build): update build logic plugins

---
+++ build-logic/convention/src/main/kotlin/mirasalon/convention/Extensions.kt
@@ -0,0 +1,42 @@
+package mirasalon.convention
+
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.LibraryExtension
+import com.android.build.api.dsl.KotlinMultiplatformAndroidLibraryTarget
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.getByType
+
+internal val Project.libs: VersionCatalog
+    get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+internal fun CommonExtension.configureAndroid(project: Project) {
+    val libs = project.libs
+
+    compileSdk = libs.findVersion("android-compileSdk").get().toString().toInt()
+
+    when (this) {
+        is LibraryExtension -> {
+            defaultConfig.minSdk = libs.findVersion("android-minSdk").get().toString().toInt()
+        }
+        is ApplicationExtension -> {
+            defaultConfig.minSdk = libs.findVersion("android-minSdk").get().toString().toInt()
+            defaultConfig.targetSdk = libs.findVersion("android-targetSdk").get().toString().toInt()
+        }
+    }
+
+    compileOptions.apply {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+internal fun KotlinMultiplatformAndroidLibraryTarget.configureAndroid(project: Project) {
+    val libs = project.libs
+
+    compileSdk = libs.findVersion("android-compileSdk").get().toString().toInt()
+    minSdk = libs.findVersion("android-minSdk").get().toString().toInt()
+}
