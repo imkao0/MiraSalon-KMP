19b7588ddb6431754c4bdbebd6ab61daf09d6e98
mkao <mkaoibrahim@gmail.com>
2025-12-04 02:50:35 +0300

    refactor(core): update network config and UI components

--- core/network/src/commonMain/kotlin/iz/mkao/mirasalon/core/network/NetworkConfig.kt
+++ core/network/src/commonMain/kotlin/iz/mkao/mirasalon/core/network/NetworkConfig.kt
@@ -2,5 +2,5 @@ package iz.mkao.mirasalon.core.network
 
 object NetworkConfig {
     const val API_BASE_URL = "http://192.168.1.113:8080/"
-    const val API_WS_URL = ""
+    const val API_WS_URL = "ws://192.168.1.113:8080/ws/notifications"
 }
